bf8057192b76625318be20ab9d203145446e58ab
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-18 22:31:31 +0000

    update task 03

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import uuid
-from datetime import datetime
+import datetime
 
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        self.updated_at = datetime.now()
-    
+        self.updated_at = datetime.datetime.now()
+
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
