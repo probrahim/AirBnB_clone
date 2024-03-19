e11e727237d18f631d2e895a657be9c463a97e6b
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-19 09:55:37 +0000

    task 4

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 0x00. AirBnB clone - The console
+
+task 0 : __
+task 1 : __
+task 2 : __
+task 3 : done
+task 4 : done
+task 5 : ....
--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        
+        if kwargs:
+            dt = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "update_at":
+                    value = datetime.strptime(kwargs[key], dt)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.isoformat("T")
+        obj_dict['updated_at'] = self.updated_at.isoformat("T")
         return obj_dict
