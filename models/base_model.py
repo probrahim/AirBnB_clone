c5e2705d2c11f2ea8220f087d64d5000899b57da
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-20 10:47:21 +0000

    task 05

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -8,20 +10,22 @@ class BaseModel:
         if kwargs:
             dt = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key == "created_at" or key == "update_at":
+                if key == 'created_at' or key == 'update_at':
                     value = datetime.strptime(kwargs[key], dt)
-                if key != "__class__":
+                if key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
+            storage.new(self)
+        
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+class FileStorage:
+    
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        met = str(obj.__class__.__name__) + '.' + str(obj.id)
+        self.__objects[met] = obj
+
+    def save(self):
+        from json import dump
+
+        file_name = self.__file_path
+
+        to_json = dict(self.__objects)
+        for k, v in to_json.items():
+            to_json[k] = v.to_dict()
+
+        with open(file_name, "w", encoding="UTF-8") as f:
+            dump(to_json, f)
+
+    def reload(self):
+        from json import load
+        try:
+            with open(self.__file_path, "r", encoding="UTF-8") as file:
+                from_json = load(file)
+                self.__objects = dict(from_json)
+        except FileNotFoundError:
+            pass
