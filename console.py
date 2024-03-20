22ada6dce5a0f609fa0f873670e353f96f86e43a
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-20 20:09:28 +0000

    30% task 7

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cmd
+import shlex
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -7,6 +9,7 @@ class HBNBCommand(cmd.Cmd):
     
     """
     prompt = "(hbnb) "
+    class_list = ["BaseModel"]
     
     def do_quit(self, arg):
         return True
@@ -23,6 +26,17 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            inst = BaseModel()
+            inst.save()
+            print(inst.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
