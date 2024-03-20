8f24e269fc4769456bc77dc83dffd7fe4edb9775
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-20 23:49:16 +0000

    show

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 import cmd
 import shlex
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -28,15 +29,34 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        commands = shlex.split(arg)
-        if len(commands) == 0:
+        cmds = shlex.split(arg)
+        if len(cmds) == 0:
             print("** class name missing **")
-        elif commands[0] not in self.class_list:
+        elif cmds[0] not in self.class_list:
             print("** class doesn't exist **")
         else:
             inst = BaseModel()
             inst.save()
             print(inst.id)
+
+    def do_show(self, arg):
+        cmds = shlex.split(arg)
+        if len(cmds) == 0:
+            print("** class name missing **")
+        elif cmds[0] is not self.class_list:
+            print("** class doesn't exist **")
+        elif len(cmds) == 1:
+            print("** instance id missing **")
+        else:
+            obj = storage.all()
+            value = "{}.{}".format(cmds[0], cmds[1])
+            if value in obj:
+                print(obj[value])
+            else:
+                print("** no instance found **")
+
+    
+            
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
